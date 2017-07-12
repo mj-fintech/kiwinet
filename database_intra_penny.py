57551a16a72466e1baf1f2271ac9dfdd97133383
Minjun Kim <minjunkim7767@gmail.com>
2017-07-12 15:48:22 -0400

    updated database for penny stocks

--- database_intra_penny.py
+++ database_intra_penny.py
@@ -1,5 +1,36 @@
 '''
 axonXaxon Database #intra #penny
 '''
-
 import pandas as pd
+import pymysql
+
+# Penny stock screener: Inputs a company ticker and outputs "False" or "True"
+# If the last sale price was less than $5.00, then we assign the stock as "Penny"
+# else it is not a "Penny"
+
+# 1. Check the price of all stocks #
+
+# 2. Make a list of Pennys #
+
+
+def isPenny(ticker):
+
+    if :
+
+        return False
+
+    else:
+
+        return True
+
+def penny_stock_list():
+
+    ticker_list = ['aapl', 'goog', 'msft', ....]
+    penny_list = []
+    for ticker in ticker_list:
+        if isPenny(ticker) == 'True':
+            penny_list.append(ticker)
+    return penny_list
+
+# 3. database work. Dynamically parse the Google Finance API and store data to
+# the MySQL database.
